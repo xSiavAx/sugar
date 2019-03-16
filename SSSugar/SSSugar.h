beb624ac06ff6b7c3020a601e3f72a7c34f65b8c
Stanislav Dmitriyev <mail@siava.pp.ua>
2019-03-16 15:23:37 +0200

    Initial Commit

---
+++ SSSugar/SSSugar.h
@@ -0,0 +1,19 @@
+//
+//  SSSugar.h
+//  SSSugar
+//
+//  Created by Stanislav Dmitriyev on 16/3/19.
+//  Copyright © 2019 Stanislav Dmitriyev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SSSugar.
+FOUNDATION_EXPORT double SSSugarVersionNumber;
+
+//! Project version string for SSSugar.
+FOUNDATION_EXPORT const unsigned char SSSugarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSSugar/PublicHeader.h>
+
+
