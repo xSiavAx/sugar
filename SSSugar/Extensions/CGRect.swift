ae10d19a9ca6b11eaba87f876d7b2f12cdf857fe
Stanislav Dmitriyev <mail@siava.pp.ua>
2019-03-18 21:23:51 +0200

    Rename inset(toX:toY) method and add inset(toSize:) method to CGRect extension

--- SSSugar/Extensions/CGRect.swift
+++ SSSugar/Extensions/CGRect.swift
@@ -11,6 +11,18 @@ public extension CGRect {
         return CGPoint(x:midX, y:midY)
     }
     
+    func inset(toSize: CGSize) -> CGRect {
+        return insetBy(dx: (width - toSize.width) / 2, dy: (height - toSize.height) / 2)
+    }
+    
+    func inset(toWidth: CGFloat = -1, toHeight: CGFloat = -1) -> CGRect {
+        let toSize = CGSize(width:toWidth == -1 ? width : toWidth,
+                            height:toHeight == -1 ? height : toHeight)
+        return inset(toSize:toSize)
+    }
+    
+    //MARK: - deprecated
+    @available(*, deprecated, message: "Use inset(toWidth:toHeight:) instead")
     func inset(toX: CGFloat = -1, toY: CGFloat = -1) -> CGRect {
         let dx = toX == -1 ? 0 : (width - toX) / 2;
         let dy = toY == -1 ? 0 : (height - toY) / 2;
