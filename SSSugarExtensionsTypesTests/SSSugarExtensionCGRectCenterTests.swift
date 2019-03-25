d69c4e5f7ea25328f323df811878d9e707006103
Stanislav Dmitriyev <mail@siava.pp.ua>
2019-03-25 20:59:27 +0200

    Fix CGRect extension's center property base test – remove useless Assert

--- SSSugarExtensionsTypesTests/SSSugarExtensionCGRectCenterTests.swift
+++ SSSugarExtensionsTypesTests/SSSugarExtensionCGRectCenterTests.swift
@@ -4,7 +4,6 @@ class SSSugarExtensionCGRectCenterTests: XCTestCase {
 
     func testCenterSquare() {
         checkCenter(0, 0, 100, 100, 50, 50)
-        XCTAssertEqual(CGRect(x:0, y:0, width:100, height:100).center, CGPoint(x:50, y:50))
     }
     //add tests for rect with width larger
     //add tests for rect with heioght larger
