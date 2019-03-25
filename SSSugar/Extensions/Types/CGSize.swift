d115c5707cdfd267c37765f0113273ceaa29431a
Stanislav Dmitriyev <mail@siava.pp.ua>
2019-03-25 20:21:36 +0200

    Add intersected method to CGSize extension

--- SSSugar/Extensions/Types/CGSize.swift
+++ SSSugar/Extensions/Types/CGSize.swift
@@ -5,12 +5,23 @@ public extension CGSize {
     ///
     /// - Warning: Method doesn't return sum of sizes but union.
     /// ```
-    /// let unitedSize = CGSize(width:100, height:50).united(with:CGSize(width:25, height:75))
+    /// let unitedSize = CGSize(width:100, height:50).united(with:CGSize(width:25, height:75)) //(100, 75)
     /// ```
     /// - Parameter size: size to union with.
     /// - Returns: Size with maximized width and height.
     func united(with size: CGSize) -> CGSize {
         return CGSize(width:max(width, size.width), height:max(height, size.height))
+    }
+    
+    /// Returns size that are intersection of subject size and passed one.
+    ///
+    /// ```
+    /// let intersectionSize = CGSize(width:100, height:50).intersected(with:CGSize(width:25, height:75)) //(25, 50)
+    /// ```
+    /// - Parameter size: size to intersect with.
+    /// - Returns: Size with minimized width and height.
+    func intersected(with size: CGSize) -> CGSize {
+        return CGSize(width:min(width, size.width), height:min(height, size.height))
     }
     
     //TODO: Add intersected
