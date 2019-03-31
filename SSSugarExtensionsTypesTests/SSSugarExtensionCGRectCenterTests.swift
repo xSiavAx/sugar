6ebdadca735a4f9f5f9649f7b371f620d7bde0ec
Stanislav Dmitriyev <mail@siava.pp.ua>
2019-03-31 15:45:47 +0300

    Add CGRect extension's center tests

--- SSSugarExtensionsTypesTests/SSSugarExtensionCGRectCenterTests.swift
+++ SSSugarExtensionsTypesTests/SSSugarExtensionCGRectCenterTests.swift
@@ -1,15 +1,41 @@
 import XCTest
 
 class SSSugarExtensionCGRectCenterTests: XCTestCase {
-
-    func testCenterSquare() {
+    func testSquare() {
         checkCenter(0, 0, 100, 100, 50, 50)
     }
-    //add tests for rect with width larger
-    //add tests for rect with heioght larger
-    //add tests for empty rect, moved empty rect
-    //add tests for moved rects – with non zero origin
-    //add tests for moved rects with negative or negative/positive origin components
+    
+    func testWidthLarger() {
+        checkCenter(0, 0, 100, 50, 50, 25)
+    }
+    
+    func testHeightLarger() {
+        checkCenter(0, 0, 50, 100, 25, 50)
+    }
+    
+    func testEmpty() {
+        checkCenter(0, 0, 0, 0, 0, 0)
+    }
+    
+    func testEmptyMoved() {
+        checkCenter(20, -50, 0, 0, 20, -50)
+    }
+    
+    func testMovedPosPos() {
+        checkCenter(20, 50, 100, 50, 70, 75)
+    }
+    
+    func testMovedPosNeg() {
+        checkCenter(20, -50, 100, 50, 70, -25)
+    }
+    
+    func testMovedNegNeg() {
+        checkCenter(-20, -60, 100, 50, 30, -35)
+    }
+    
+    func testMovedNegPos() {
+        checkCenter(-60, 50, 100, 50, -10, 75)
+    }
 
     func checkCenter(_ x: CGFloat, _ y: CGFloat, _ w:CGFloat, _ h:CGFloat, _ eX:CGFloat, _ eY:CGFloat) {
         XCTAssertEqual(CGRect(x:x, y:y, width:w, height:h).center, CGPoint(x:eX, y:eY))
