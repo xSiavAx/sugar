7f8a63585615924c109d2e4a85fccd2e1c0ceca9
Stanislav Dmitriyev <mail@siava.pp.ua>
2019-04-07 15:07:17 +0300

    Migrate to swift 5.0

--- SSSugar/SSSugar.h
+++ SSSugar/SSSugar.h
@@ -9,6 +9,7 @@ FOUNDATION_EXPORT const unsigned char SSSugarVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <SSSugar/PublicHeader.h>
 
 //TODO: Write Array tests
+//TODO: Write Array docs
 
 //TODO: Write docs and test for all extensions and classes
 //TODO: Implements "Sugar" from UkrNet
