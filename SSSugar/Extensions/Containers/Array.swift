64265e260189df4ef68c1f4af527262b6d4ee1b5
Stanislav Dmitriyev <mail@siava.pp.ua>
2019-04-14 18:08:25 +0300

    Add binarySearch(forInsert:) to Array extension

--- SSSugar/Extensions/Containers/Array.swift
+++ SSSugar/Extensions/Containers/Array.swift
@@ -2,27 +2,22 @@ import Foundation
 
 public extension Array {
     init(size: Int, buildBlock:(Int)->(Element)) {
-        #warning("Fix me")
+        #warning("Swift 5.1")
         //FIXME: Replace by new swift 5.1 array constructor
         self.init((0..<size).map(buildBlock))
     }
     
     func binarySearch(_ needle: Element, comparator: (Element, Element)->ComparisonResult) -> Int? {
-        var range = 0..<count
-        
-        while !range.isEmpty {
-            let middle = range.middle
+        return bSearchIdxAndLastIdx(needle, comparator: comparator).0
+    }
+    
+    func binarySearch(forInsert needle: Element, comparator: (Element, Element)->ComparisonResult) -> Int {
+        let lastIDx = bSearchIdxAndLastIdx(needle, comparator: comparator).1
 
-            switch comparator(needle, self[middle]) {
-            case .orderedSame:
-                return middle
-            case .orderedAscending:
-                range = range.prefix(upTo: middle)
-            case .orderedDescending:
-                range = range.suffix(from: middle + 1)
-            }
+        if (count > 0 && comparator(self[lastIDx], needle) == .orderedAscending) {
+            return lastIDx + 1
         }
-        return nil
+        return lastIDx
     }
     
     func forEach(_ body: (Element, Int) throws -> Void) rethrows {
@@ -40,10 +35,34 @@ public extension Array {
     static func array(size: Int, buildBlock:(Int)->(Element)) -> Array<Element> {
         return (0..<size).map(buildBlock)
     }
+    
+    //MARK: - private
+    private func bSearchIdxAndLastIdx(_ needle: Element, comparator: (Element, Element)->ComparisonResult) -> (Int?, Int) {
+        var range = 0..<count
+        var lastIDx = 0
+        
+        while !range.isEmpty {
+            lastIDx = range.middle
+            
+            switch comparator(needle, self[lastIDx]) {
+            case .orderedSame:
+                return (lastIDx, lastIDx)
+            case .orderedAscending:
+                range = range.prefix(upTo: lastIDx)
+            case .orderedDescending:
+                range = range.suffix(from: lastIDx + 1)
+            }
+        }
+        return (nil, lastIDx)
+    }
 }
 
 public extension Array where Element : Comparable {
     func binarySearch(_ needle: Element) -> Int? {
         return binarySearch(needle) {$0.compare($1)}
+    }
+    
+    func binarySearch(forInsert needle: Element) -> Int {
+        return binarySearch(forInsert:needle) {$0.compare($1)}
     }
 }
