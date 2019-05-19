09fae1ec290a097b4277d0ef75524566384eecf6
Stanislav Dmitriyev <mail@siava.pp.ua>
2019-05-19 18:29:19 +0300

    Add count and mutating methods returns to AutoMap

--- SSSugar/Classes/Containers/AutoMap.swift
+++ SSSugar/Classes/Containers/AutoMap.swift
@@ -2,34 +2,80 @@ import Foundation
 
 public protocol ReplaceableCollection : Collection where Element : Equatable {
     init()
-    mutating func insert(e: Element)
-    mutating func remove(e: Element)
+    @discardableResult mutating func insert(e: Element) -> Bool
+    @discardableResult mutating func remove(e: Element) -> Bool
 }
 
 public struct AutoMap<Key : Hashable, Container : ReplaceableCollection> {
     public typealias Value = Container.Element
+    private (set) var count = 0
     private var map = [Key : Container]()
     
-    mutating func add(key: Key, _ element: Value) {
+    mutating func add(key: Key, _ element: Value) -> Bool {
         if map[key] == nil {
             map[key] = Container()
         }
-        map[key]!.insert(e:element)
+        if (map[key]!.insert(e:element)) {
+            count += 1
+            return true
+        }
+        return false
     }
     
-    mutating func remove(key: Key, element: Value) {
-        map[key]?.remove(e:element)
-        if (map[key]?.count == 0) {
-            map.removeValue(forKey: key)
+    subscript(key: Key) -> Container? {
+        get {
+            return map[key]
+        }
+        set {
+            if let container = newValue {
+                do {
+                    try replace(key: key, container: container)
+                } catch {
+                    fatalError(error.localizedDescription)
+                }
+            } else {
+                remove(key: key)
+            }
+
         }
     }
     
-    mutating func remove(key: Key) {
-        map.removeValue(forKey: key)
+    @discardableResult mutating func remove(key: Key, element: Value) -> Bool {
+        if (map[key]?.remove(e:element) ?? false) {
+            if (map[key]?.count == 0) {
+                map.removeValue(forKey: key)
+            }
+            count -= 1
+            return true
+        }
+        return false
     }
     
-    mutating func removeAll() {
+    @discardableResult mutating func remove(key: Key) -> Container? {
+        if let container = map.removeValue(forKey: key) {
+            count -= container.count
+            return container
+        }
+        return nil
+    }
+    
+    @discardableResult mutating func removeAll() -> [Container] {
+        let containers = map.values
+        
         map.removeAll()
+        count = 0
+        return Array(containers)
+    }
+    
+    @discardableResult mutating func replace(key: Key, container: Container) throws -> Container? {
+        guard container.count > 0 else {
+            throw AutoMapError.invalidUsage
+        }
+        let oldContainer = remove(key: key)
+        
+        map[key] = container
+        count += container.count
+        return oldContainer
     }
 }
 
@@ -73,7 +119,7 @@ extension AutoMap : Sequence {
     }
 }
 
-extension AutoMap where Container : RangeReplaceableCollection {
+extension AutoMap where Container : RangeReplaceableCollection & MutableCollection {
     typealias Index = Container.Index
     
     subscript(key: Key, index: Index) -> Value? {
@@ -82,22 +128,32 @@ extension AutoMap where Container : RangeReplaceableCollection {
         }
         set {
             if let mValue = newValue {
-                insert(key: key, at: index, element: mValue)
+                update(key: key, at: index, element: mValue)
             } else {
                 remove(key: key, at: index)
             }
         }
     }
     
-    private mutating func insert(key: Key, at index: Container.Index, element: Value) {
+    mutating func insert(key: Key, at index: Container.Index, element: Value) {
         if map[key] == nil {
             map[key] = Container()
         }
         map[key]!.insert(element, at: index)
+        count += 1;
+    }
+    
+    mutating func update(key: Key, at index: Container.Index, element: Value) {
+        if map[key] == nil {
+            map[key] = Container()
+        }
+        map[key]![index] = element
+        count += 1;
     }
     
     private mutating func remove(key: Key, at: Container.Index) {
         map[key]?.remove(at: at)
+        count -= 1
         if (map[key]?.count == 0) {
             map.removeValue(forKey: key)
         }
@@ -108,23 +164,31 @@ extension AutoMap : CustomStringConvertible {
     public var description: String { return "\(map)" }
 }
 
+extension AutoMap {
+    //TODO: Add cases
+    enum AutoMapError: Error {
+        case invalidUsage
+    }
+}
+
 extension Array : ReplaceableCollection where Element : Equatable {
-    public mutating func insert(e: Element) {
+    public mutating func insert(e: Element) -> Bool {
         append(e)
+        return true
     }
 
-    public mutating func remove(e: Element) {
+    public mutating func remove(e: Element) -> Bool {
         remove(at: firstIndex(of: e)!)
+        return true
     }
 }
 
 extension Set : ReplaceableCollection {
-    public mutating func insert(e: Element) {
-        insert(e)
+    public mutating func insert(e: Element) -> Bool {
+        return insert(e).inserted
     }
     
-    public mutating func remove(e: Element) {
-        remove(e)
+    public mutating func remove(e: Element) -> Bool {
+        return remove(e) != nil
     }
 }
-
