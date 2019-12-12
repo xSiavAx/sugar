6f9453bd8bd55527200d6bd6bd55de27af2c26b5
Stanislav Dmitriyev <mail@siava.pp.ua>
2019-12-12 19:32:58 +0200

    Correct TODO list

--- SSSugar/SSSugar.h
+++ SSSugar/SSSugar.h
@@ -10,12 +10,10 @@ FOUNDATION_EXPORT const unsigned char SSSugarVersionString[];
 
 //TODO: Add architecture components (like Display, Adapter, State, Presenter) based on protocols
 
-//TODO: Add concurrent async jobs synchroniser based on dispatch group.
-//TODO: Add non-concurent async jobs synchroniser. (execute jobs one by one)
-
 //TODO: Fix CGSize.extended(by size:)
-//TODO: Write AutoMap containers tests
 //TODO: Add dict 'pick' and 'picking' tests and documentation for Array and Dict
+//TODO: Add SSGroupExecutor and SSChainExecutor tests and documentation
+//TODO: Write AutoMap containers tests
 //TODO: Add DataBase tests
 //TODO: Add default values '0' to 'extended' method inside CGSize extension
 //TODO: Write docs and test for 'added' and 'extended' methods inside CGSize extension
