e2be0ef66744c894e578ac8fcb0b70f13878b3ee
Stanislav Dmitriyev <mail@siava.pp.ua>
2019-12-24 16:49:12 +0200

    Add SSViewTransitionAnimating and apply it to SSRootView

---
+++ SSSugar/Classes/View Transition Animators/SSViewTransitionAnimating.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+public protocol SSViewTransitionContaining {
+    /// Frame that content view should fill on animation finish
+    func frameForContent() -> CGRect
+}
+
+public protocol SSViewTransitionAnimating {
+    func transition(from oldView: UIView, to newView: UIView, by container: SSViewTransitionContaining, onFinish: @escaping (UIViewAnimatingPosition)->Void)
+}
--- SSSugar/Classes/ViewControllers/Root/SSRootView.swift
+++ SSSugar/Classes/ViewControllers/Root/SSRootView.swift
@@ -1,60 +1,46 @@
 import UIKit
 
 open class SSRootView: UIView {
-    static private let kDefaultTransitionDuration = 0.25;
     private var contentView : UIView
-    private let transitionDuration : TimeInterval
+    public let animator: SSViewTransitionAnimating
     
     public required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    init(withContentView contentView: UIView, transitionDuration mTransitionDuration: TimeInterval = kDefaultTransitionDuration) {
-        self.contentView = contentView
-        self.transitionDuration = mTransitionDuration
+    public init(withContentView: UIView, animator mAnimator: SSViewTransitionAnimating = SSScaleAndAlphaTransitionAnimator()) {
+        contentView = withContentView
+        animator = mAnimator
         super.init(frame: .zero)
         
         addSubview(contentView)
     }
     
-    func replaceContentView(_ newView: UIView, animated : Bool = false) {
+    public func replaceContentView(_ newView: UIView, animated : Bool = false) {
         let oldView = contentView;
         
         contentView = newView;
         insertSubview(contentView, at: 0)
         
-        contentView.frame = bounds
-        
         if (animated) {
-            hideView(view: contentView)
-            UIView.animate(withDuration: self.transitionDuration,
-                           animations: {
-                            [unowned self] in
-                            self.showView(view: self.contentView)
-                            self.hideView(view: oldView)
-            },
-                           completion: { (finished) in
-                            oldView.removeFromSuperview()
-            })
-            
+            animator.transition(from: oldView, to: contentView, by: self) { (position) in
+                oldView.removeFromSuperview()
+            }
             
         } else {
+            contentView.frame = bounds
             oldView.removeFromSuperview()
         }
     }
     
     open override func layoutSubviews() {
         super.layoutSubviews()
-        contentView.frame = bounds
+        contentView.frame = frameForContent()
     }
-    
-    private func hideView(view : UIView) {
-        view.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
-        view.alpha = 0
-    }
-    
-    private func showView(view : UIView) {
-        view.transform = CGAffineTransform(scaleX: 1, y: 1)
-        view.alpha = 1
+}
+
+extension SSRootView: SSViewTransitionContaining {
+    public func frameForContent() -> CGRect {
+        return bounds
     }
 }
