69bd64001064e5e9c06b54c0dffdc1828c7e07ed
Stanislav Dmitriyev <mail@siava.pp.ua>
2019-12-24 13:58:16 +0200

    Add sum and diff for CGSize and CGPoint

---
+++ SSSugar/Extensions/Types/CGPoint.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+//TODO: Add docs and tests
+
+public prefix func -(point: CGPoint) -> CGPoint {
+    return CGPoint(x: -point.x, y: -point.y)
+}
+
+public func +(lPoint: CGPoint, rPoint: CGPoint) -> CGPoint {
+    return CGPoint(x: lPoint.x + rPoint.x, y: lPoint.y + rPoint.y)
+}
+
+public func -(lPoint: CGPoint, rPoint: CGPoint) -> CGPoint {
+    return lPoint + (-rPoint)
+}
