0ee8fe41b40f1ac9412322b9720fadbb97579e5b
Stanislav Dmitriyev <mail@siava.pp.ua>
2019-12-27 14:14:17 +0200

    Add maxSizeThatFits to UILabel

--- SSSugar/Extensions/Views/UILabel.swift
+++ SSSugar/Extensions/Views/UILabel.swift
@@ -4,7 +4,7 @@ extension UILabel {
     //TODO: Add tests
     /// Ask view to calculate it's possible size best fits the specified size.
     ///
-    ///  If label has content, it works as usual sizeThatFits, otherwise it works like sizeThatFits for label with space symbol as it's content (' ').
+    /// If label has content, it works as usual sizeThatFits, otherwise it works like sizeThatFits for label with space symbol as it's content (' ').
     ///
     /// - Parameter size: Size to fit.
     /// - Returns: Possible size to fit passed one.
@@ -18,9 +18,31 @@ extension UILabel {
     }
     
     //TODO: Add tests
+    /// Ask view to calculate it's max possible (like it has max number of lines content) size best fits the specified size.
+    ///
+    /// - Warning:
+    /// If label's `numberOfLines` is equal to 0, method works like regular `sizeThatFits`
+    ///
+    /// - Parameters:
+    ///   - size: Size to fit.
+    /// - Returns: Possible size to fit passed one.
+    public func maxSizeThatFits(_ size: CGSize) -> CGSize {
+        let size = sizeThatFits(size)
+        
+        if (numberOfLines != 0) {
+            let text = Array(repeating: " ", count: numberOfLines).joined(separator: "\n")
+            let maxSize = sizeThatFits(size, withText: text)
+            
+            return maxSize.height > size.height ? maxSize : size;
+        }
+        return size
+    }
+    
+    //TODO: Add tests
     /// Ask view to calculate it's estimmated (for passed text) size best fits the specified size.
     ///
-    ///  It works like regular sizeThatFits for label with passed `text` as it's content (' ').
+    ///  It works like regular sizeThatFits for label with passed `text` as it's content.
+    ///
     /// - Parameters:
     ///   - size: Size to fit.
     ///   - withText: Text to mesure
