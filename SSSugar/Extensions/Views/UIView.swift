dd82746c12b878fd5984d5629f2dff9046afe1c7
Stanislav Dmitriyev <mail@siava.pp.ua>
2020-01-08 18:27:59 +0200

    Add adding array of subviews method to UIView

--- SSSugar/Extensions/Views/UIView.swift
+++ SSSugar/Extensions/Views/UIView.swift
@@ -15,6 +15,12 @@ public extension UIView {
         }
     }
     
+    func add(subviews : [UIView]) {
+        subviews.forEach { (subview) in
+            addSubview(subview)
+        }
+    }
+    
     func hasParent(_ needle: UIView) -> Bool {
         var view = self
         
@@ -25,24 +31,6 @@ public extension UIView {
             view = sView
         }
         return false
-    }
-}
-
-extension UIView: SSViewConfigReplacable {
-    /// Replace current view config by new one.
-    /// Call `setConfig` for storing config and `configWith` to immediately config view. Override these 2 methods if you want store or apply passed config.
-    /// - Warning: This method will automatically call 'replaceConfig' for all current subviews, don't do it on your own. Usually there is no reason to override this method at all.
-    /// - Complexity: Time O(N), N - total subviews count and Space O(K), K - deepest subview's tree branch
-    ///
-    /// - Parameter config: Object that may be used for view configuration
-    public func replaceConfig(_ config: Any) {
-        if let configurable = self as? SSViewConfigurable {
-            configurable.storeConfig(config)
-            configurable.configWith(config)
-        }
-        for subview in subviews {
-            subview.replaceConfig(config)
-        }
     }
 }
 
