e1d5dfa28d1d5a255b684ef17f0e94a955f41b3f
Stanislav <mail@siava.pp.ua>
2020-04-27 12:47:28 +0300

    Remove incomplete task TODOs

--- SSSugar/Utils/Data Synchronisation/Revisions/SSDataModifying.swift
+++ SSSugar/Utils/Data Synchronisation/Revisions/SSDataModifying.swift
@@ -15,10 +15,6 @@ extension SSDataModifying {
     }
 }
 
-//TODO: Rename SSDmChange and SSDmRequest to SSDmFinalChange and SSDmFinalRequest
-//TODO: Make SSDmChange and SSDmRequest not conforming to SSDataModifying. It allows use em in composites to specify SSDataModifying is Change or Request. Move static 'title' to these protocols (from 'final' ones).
-//TODO: Make SSDmFinalChange and SSDmFinalRequest as composites of SSDmChange and SSDmRequest with SSDataModifying
-
 public protocol SSDmChange: SSDataModifying {
     static var title: String {get}
 }
