984fd1650b0439b878ce12dea064d5eb6619ea52
Stanislav <mail@siava.pp.ua>
2020-05-05 16:20:21 +0300

    Add docs for SSTry

--- SSSugar/Classes/SSTry.swift
+++ SSSugar/Classes/SSTry.swift
@@ -1,15 +1,17 @@
 import Foundation
 
+/// Class helper simplifies do/try/catch code to optional error returning.
 public class SSTry {
+    /// Perform passed job and return error if one has thrown
+    /// - Parameter job: Job to perform
+    /// - Returns: Error if has thrown during job performing
     public static func run(job: () throws ->Void ) -> Error? {
-        do {
-            try job()
-            return nil
-        } catch {
-            return error
-        }
+        return cast(job: job)
     }
     
+    /// Perform passed job and return casted error if one has thrown
+    /// - Parameter job: Job to perform
+    /// - Returns: Casted error if has thrown during job performing
     public static func cast<CError: Error>(job: () throws ->Void ) -> CError? {
         do {
             try job()
