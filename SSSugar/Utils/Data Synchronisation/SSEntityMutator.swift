c1dafaa0246ccff16eb65ad8e87116c7bc679969
Stanislav <mail@siava.pp.ua>
2020-05-18 15:35:13 +0300

    Add docs for Entity Updater and Mutator

--- SSSugar/Utils/Data Synchronisation/SSEntityMutator.swift
+++ SSSugar/Utils/Data Synchronisation/SSEntityMutator.swift
@@ -1,9 +1,12 @@
 import Foundation
 
-/// Requirements for entity source for SSEntityMutator
+/// Requirements for Mutator's Entity source.
 public protocol SSMutatingEntitySource: AnyObject {
+    /// Source Entity type
     associatedtype Entity
     
+    /// Returns entity for Mutator
+    /// - Parameter updater: Mutator that requiries entity.
     func entity<Mutating: SSBaseEntityMutating>(for mutator: Mutating) -> Entity?
 }
 
@@ -16,14 +19,25 @@ extension SSMarkerGenerating {
     public static func newMarker() -> String { return UUID().uuidString }
 }
 
-/// Requirements for Entity Mutator with some addons (like marker generating or dispatching on main queue)
+/// Requirements for Entity Mutator with some addons (like marker generating or dispatching on main queue).
+///
+/// # Implemets:
+/// `SSMarkerGenerating`, `SSOnMainExecutor`
 public protocol SSBaseEntityMutating: SSMarkerGenerating, SSOnMainExecutor {
+    /// Mutator's entity Source Type
     associatedtype Source: SSMutatingEntitySource
+    /// Mutator's Entity Type (`Source.Entity` shortcut)
     typealias Entity = Source.Entity
     
     /// Object that give's (and potentially own) entity object to mutate.
     var source: Source? {get}
     
+    /// Starts `Updater`.
+    /// - Parameter source: Entity source.
+    ///
+    /// Implementation should include source assigning.
     func start(source: Source)
+    
+    /// Stops `Updater`.
     func stop()
 }
--- SSSugar/Utils/Data Synchronisation/SSEntityUpdater.swift
+++ SSSugar/Utils/Data Synchronisation/SSEntityUpdater.swift
@@ -1,24 +1,51 @@
 import Foundation
 
+/// Requirements for Updater's Entity source.
 public protocol SSUpdaterEntitySource: AnyObject {
+    /// Source Entity type
     associatedtype Entity
-
+    
+    /// Returns entity for Updater
+    /// - Parameter updater: Updater that requiries entity.
     func entity<Updater: SSBaseEntityUpdating>(for updater: Updater) -> Entity?
 }
 
+/// Base protocol for Updater's delegate
 public protocol SSEntityUpdaterDelegate: AnyObject {}
 
-/// Requirements for Entity Mutator with some addons (like marker generating or dispatching on main queue)
+/// Requirements for Entity Updater
+///
+/// # Provides:
+/// * `entity` implementation via `source.entity` proxing.
+/// * `func start(source: Source, delegate: Delegate)` implementation.
+/// * `func stop()` implementation.
+///
+/// # Implements:
+/// `SSOnMainExecutor`
 public protocol SSBaseEntityUpdating: AnyObject, SSOnMainExecutor {
+    /// Updater's entity Source Type
     associatedtype Source: SSUpdaterEntitySource
+    /// Updater's Delegate Type
     associatedtype Delegate: SSEntityUpdaterDelegate
     
-    /// Object that give's (and potentially own) entity object to mutate.
+    /// Object that give's (and potentially own) entity object to update.
     var source: Source? {get set}
+    /// Updater's delegate
     var delegate: Delegate? {get set}
+    /// Manager to subscribe/unsubscribe to/from updates.
     var receiversManager: SSUpdateReceiversManaging {get}
     
+    /// Startes updater.
+    ///
+    /// # Default implementation:
+    /// Assign `source` and `delegate`, subscribe for updates.
+    /// - Parameters:
+    ///   - source: Entity source.
+    ///   - delegate: Entity updater's delegate.
     func start(source: Source, delegate: Delegate)
+    /// Stops updater.
+    /// # Default implementation:
+    /// Unsubscribe from updates.
     func stop()
 }
 
