a246e88fb514e6049f1a0fa5330fe5254bbff040
Stanislav <mail@siava.pp.ua>
2020-05-20 12:06:55 +0300

    Add Initial Update Center tests

---
+++ SSSugarTests/Utils/Data Synchronisation/SSUpdateCenterTests.swift
@@ -0,0 +1,9 @@
+//
+//  SSUpdateCenterTests.swift
+//  SSSugarTests
+//
+//  Created by Stanislav Dmitriyev on 19.05.2020.
+//  Copyright © 2020 Stanislav Dmitriyev. All rights reserved.
+//
+
+import Foundation
