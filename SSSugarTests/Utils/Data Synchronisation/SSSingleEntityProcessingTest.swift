7dc03a5d31b91875f7c5add8f58d2de09be487de
Stanislav <mail@siava.pp.ua>
2020-05-20 16:08:09 +0300

    Add SingleEntityProcessing tests

---
+++ SSSugarTests/Utils/Data Synchronisation/SSSingleEntityProcessingTest.swift
@@ -0,0 +1,9 @@
+//
+//  SSSingleEntityProcessingTest.swift
+//  SSSugarTests
+//
+//  Created by Stanislav Dmitriyev on 20.05.2020.
+//  Copyright © 2020 Stanislav Dmitriyev. All rights reserved.
+//
+
+import Foundation
