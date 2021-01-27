53b767b44de84f62b78816aed8d35fe104bf5ffc
Stanislav <mail@siava.pp.ua>
2021-01-27 13:48:32 +0200

    Fix prev crap commit

--- SSSugar/Utils/Key Coding/Adapters/RawRepresentable.swift
+++ SSSugar/Utils/Key Coding/Adapters/RawRepresentable.swift
@@ -1,9 +1,29 @@
-//
-//  RawRepresentable.swift
-//  SSSugar
-//
-//  Created by Stanislav Dmitriyev on 27.01.2021.
-//  Copyright © 2021 Stanislav Dmitriyev. All rights reserved.
-//
-
 import Foundation
+
+//TODO: See how we could upgrade SSErrorConverter and SSApiErrorConverter using RawRepresentable.
+// For that purpouses we could add `to`, `from`, `read` and `write` to RawRepresentable.
+public extension RawRepresentable {
+    typealias KeyFieldOptAdapter = SSKeyField<Self?>.Adapter
+    typealias KeyFieldAdapter = SSKeyField<Self>.Adapter
+    
+    static func keyFieldOptAdapter() -> KeyFieldOptAdapter {
+        func to(val: Any?) -> Self? {
+            guard let raw = val as? RawValue else { return nil }
+            return Self(rawValue: raw)
+        }
+        func from(val: Self?) -> Any? {
+            return val?.rawValue
+        }
+        return KeyFieldOptAdapter(to: to, from: from)
+    }
+    
+    static func keyFieldAdapter() -> KeyFieldAdapter {
+        func to(val: Any?) -> Self {
+            return Self(rawValue: val as! RawValue)!
+        }
+        func from(val: Self) -> Any? {
+            return val.rawValue
+        }
+        return KeyFieldAdapter(to: to, from: from)
+    }
+}
