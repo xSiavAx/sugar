bc96a0be7c8ef7210ab75f5b08b2dd18173c8207
Stanislav <mail@siava.pp.ua>
2021-01-27 13:48:23 +0200

    Add RawRepresentable to Key Coding

---
+++ SSSugar/Utils/Key Coding/Adapters/RawRepresentable.swift
@@ -0,0 +1,9 @@
+//
+//  RawRepresentable.swift
+//  SSSugar
+//
+//  Created by Stanislav Dmitriyev on 27.01.2021.
+//  Copyright © 2021 Stanislav Dmitriyev. All rights reserved.
+//
+
+import Foundation
