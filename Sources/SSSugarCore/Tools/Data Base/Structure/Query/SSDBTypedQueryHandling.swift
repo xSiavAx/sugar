6df62d304ad81078f270c7b6e46b6b3da8511947
Stanislav <mail@siava.pp.ua>
2021-06-14 17:12:49 +0300

    Remove binding from arg-less commit in SSDBTypedQueryHandling

--- Sources/SSSugarCore/Tools/Data Base/Structure/Query/SSDBTypedQueryHandling.swift
+++ Sources/SSSugarCore/Tools/Data Base/Structure/Query/SSDBTypedQueryHandling.swift
@@ -37,13 +37,7 @@ public extension SSDBTypedQueryHandling {
     }
     
     func commit(db: SSDataBaseProtocol, args: [BArgs], preBind: PreBind? = nil) throws {
-        try withStmt(db: db) {(stmt) in
-            try preBind?(stmt)
-            try args.forEach {
-                try stmt.bind(args: $0)
-                try stmt.commit()
-            }
-        }
+        try commit(db: db, args: args, preBind: preBind) { try $0.bind(args: $1) }
     }
     
     func select(db: SSDataBaseProtocol, args: BArgs) throws -> GArgs? {
@@ -70,6 +64,16 @@ public extension SSDBTypedQueryHandling {
             return result
         }
     }
+    
+    private func commit(db: SSDataBaseProtocol, args: [BArgs], preBind: PreBind? = nil, bind: ((TypedStmt, BArgs) throws -> Void)?) throws {
+        try withStmt(db: db) {(stmt) in
+            try preBind?(stmt)
+            try args.forEach {
+                try bind?(stmt, $0)
+                try stmt.commit()
+            }
+        }
+    }
 }
 
 public extension SSDBTypedQueryHandling where Stmt == SSDataBaseStatementProxy {
@@ -84,8 +88,8 @@ public extension SSDBTypedQueryHandling where Stmt == SSDataBaseStatementProcess
     }
 }
 
-extension SSDBTypedQueryHandling where BArgs == Void {
-    func commit(db: SSDataBaseProtocol) throws {
-        try commit(db: db, args: ())
+public extension SSDBTypedQueryHandling where BArgs == Void {
+    func commit(db: SSDataBaseProtocol, preBind: PreBind? = nil) throws {
+        try commit(db: db, args: [()], preBind: preBind, bind: nil)
     }
 }
