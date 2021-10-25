e1b4c203647bcedda6dea238d61bacbaead8a99c
Stanislav Dmitriyev <mail@siava.pp.ua>
2021-10-25 17:10:26 +0300

    Make SSAtomic property wrapper

---
+++ Tests/SSSugarCoreTests/Playgorund/SSSugarCorePlaygroundTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+import Foundation
+
+@testable import SSSugarCore
+
+class SSSugarCorePlaygroundTests: XCTestCase {
+    
+    func testMain() {
+
+    }
+    
+    
+    func testSecond() {
+    }
+}
