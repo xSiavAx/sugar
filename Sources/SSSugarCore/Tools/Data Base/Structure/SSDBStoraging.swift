af5c50e2e7bff1df4f8afc056da7a20ff5515650
Nikita Borysov <nikitaborysov@me.com>
2021-12-13 17:34:14 +0200

    Split SSDBStoraging protocol

--- Sources/SSSugarCore/Tools/Data Base/Structure/SSDBStoraging.swift
+++ Sources/SSSugarCore/Tools/Data Base/Structure/SSDBStoraging.swift
@@ -1,21 +1,9 @@
 import Foundation
 
+public typealias SSDBStoragingContainer = SSDBStoraging & SSDBStoragingTabled & SSDBStoragingInitialize & SSDBStoragingDeinitialize & SSDBStoragingTransacted
+
 public protocol SSDBStoraging {
     var db: SSDataBaseStorage { get }
-    
-    static var tables: [SSDBTable.Type] {get}
-    
-    func initializeStructure() throws
-    
-    func initializeStructure(strictExist: Bool) throws
-    
-    func deinitializeStructure() throws
-    
-    func deinitializeStructure(strictExist: Bool) throws
-    
-    func withinTransaction<T>(job: () throws -> T ) throws -> T
-    
-    func withinSavePoint<T>(_ label: String, job: () throws -> T) throws -> T
     
     func within<T, Transaction>(create: () throws -> Transaction,
                                 cancel: (Transaction) throws -> Void,
@@ -23,48 +11,71 @@ public protocol SSDBStoraging {
                                 job: () throws -> T) throws -> T
 }
 
-public extension SSDBStoraging {
+public protocol SSDBStoragingTabled: SSDBStoraging {
+    static var tables: [SSDBTable.Type] { get }
+}
+
+public protocol SSDBStoragingInitialize: SSDBStoragingTabled {
+    func initializeStructure() throws
+    func initializeStructure(strictExist: Bool) throws
+}
+
+public extension SSDBStoragingInitialize {
     func initializeStructure() throws {
         try initializeStructure(strictExist: true)
     }
-    
+
     func initializeStructure(strictExist: Bool) throws {
         let queries = Self.tables.reduce([]) { $0 + $1.createQueries(strictExist: strictExist) }
-        
+
         try db.exec(queries: queries)
     }
-    
+}
+
+public protocol SSDBStoragingDeinitialize: SSDBStoragingTabled {
+    func deinitializeStructure() throws
+    func deinitializeStructure(strictExist: Bool) throws
+}
+
+public extension SSDBStoragingDeinitialize {
     func deinitializeStructure() throws {
         try deinitializeStructure(strictExist: true)
     }
-    
+
     func deinitializeStructure(strictExist: Bool) throws {
         let queries = Self.tables.reduce([]) { $0 + $1.dropQueries(strictExist: strictExist) }
-        
+
         try db.exec(queries: queries)
     }
-    
+}
+
+public protocol SSDBStoragingTransacted: SSDBStoraging {
+    func withinTransaction<T>(job: () throws -> T ) throws -> T
+    func withinSavePoint<T>(_ label: String, job: () throws -> T) throws -> T
+}
+
+public extension SSDBStoragingTransacted {
     func withinTransaction<T>(job: () throws -> T ) throws -> T {
         try within(create: { try db.beginTransaction() },
                    cancel: { try db.cancelTransaction() },
                    commit: { try db.commitTransaction() },
                    job: job)
     }
-    
+
     func withinSavePoint<T>(_ label: String, job: () throws -> T) throws -> T {
         try within(create: { try db.savePoint(withTitle: label) },
                    cancel: { try $0.rollBack(); try $0.release() },
                    commit: { try $0.release() },
                    job: job)
     }
-    
-    func within<T, Transaction>(create: () throws -> Transaction,
+
+    private func within<T, Transaction>(create: () throws -> Transaction,
                                 cancel: (Transaction) throws -> Void,
                                 commit: (Transaction) throws -> Void,
                                 job: () throws -> T) throws -> T {
         let transaction = try create()
         var result: T
-        
+
         do {
             result = try job()
         } catch {
