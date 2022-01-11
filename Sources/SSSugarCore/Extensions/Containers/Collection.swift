2e88a4636a7ba633320b77c812452d86440a2246
Stanislav Dmitriyev <mail@siava.pp.ua>
2022-01-11 19:49:51 +0200

    Add evens() and odds() to collection

--- Sources/SSSugarCore/Extensions/Containers/Collection.swift
+++ Sources/SSSugarCore/Extensions/Containers/Collection.swift
@@ -1,4 +1,16 @@
 import Foundation
 
-public extension Collection {
+public extension Collection where Index == Int {
+    func evens() -> [Element] {
+        StepSequence.l(0, count, 2).map() { even in
+            return self[even]
+        }
+    }
+    
+    func odds() -> [Element] {
+        StepSequence.l(1, count, 2).map() { odd in
+            return self[odd]
+        }
+    }
 }
+
