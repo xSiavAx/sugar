ca16d0104a4cd09de1fb2a595935acf4e4cd38a1
Stanislav Dmitriyev <mail@siava.pp.ua>
2022-01-11 15:47:25 +0200

    Add map(_ b: (_ idx: _ el:), compactMap(_ b: (_ idx: _ el:) and filter(_ b: (_ idx: _ el:) to Sequence extension

--- Sources/SSSugarCore/Extensions/Containers/Sequence.swift
+++ Sources/SSSugarCore/Extensions/Containers/Sequence.swift
@@ -11,10 +11,33 @@ public extension Sequence {
     /// - Note: Method do the same as `forEach(_ body: (Element) throws -> Void)` but additionally pass index of object to given closure. See it's documentation for more details.
     func forEach(_ body: (_ idx: Int, _ element: Element) throws -> Void) rethrows {
         var idx = 0
-        
-        try self.forEach { (element) in
+        try self.forEach {
             defer { idx += 1 }
-            try body(idx, element)
+            try body(idx, $0)
+        }
+    }
+    
+    func map<T>(_ build: (Int, Element) throws -> T) rethrows -> [T] {
+        var idx = 0
+        return try map() { element in
+            defer { idx += 1 }
+            return try build(idx, element)
+        }
+    }
+
+    func compactMap<T>(_ build: (Int, Element) throws -> T) rethrows -> [T] {
+        var idx = 0
+        return try compactMap() { element in
+            defer { idx += 1}
+            return try build(idx, element)
+        }
+    }
+
+    func filter(_ isIncluded: (Int, Element) throws -> Bool) rethrows -> [Element] {
+        var idx = 0
+        return try filter() { el in
+            defer { idx += 1 }
+            return try isIncluded(idx, el)
         }
     }
 }
