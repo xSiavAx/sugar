a123e25813ed9fc0d18f510a2ebdfe6caf87f89b
Stanislav Dmitriyev <mail@siava.pp.ua>
2022-01-12 16:10:39 +0200

    Fix args order in forEach usage

--- Tests/SSSugarCoreTests/Extensions/Containers/Array/ArrayForEachTests.swift
+++ Tests/SSSugarCoreTests/Extensions/Containers/Array/ArrayForEachTests.swift
@@ -5,7 +5,7 @@ class ArrayForEachTests: XCTestCase {
         let arr = (0...10).map() { return $0 * 2 }
         var expectedIdx = 0
         
-        arr.forEach { (element, idx) in
+        arr.forEach { (idx, Element) in
             XCTAssertEqual(expectedIdx, idx)
             XCTAssertTrue(element == arr[idx])
             
