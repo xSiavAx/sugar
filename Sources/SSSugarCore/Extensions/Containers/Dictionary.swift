a715bdad2de8f348ed80a194a28f6f0d193e062a
Stanislav Dmitriyev <mail@siava.pp.ua>
2022-01-12 13:31:35 +0200

    Add remove(sequence:) to Dictionary

--- Sources/SSSugarCore/Extensions/Containers/Dictionary.swift
+++ Sources/SSSugarCore/Extensions/Containers/Dictionary.swift
@@ -9,21 +9,41 @@ public extension Dictionary {
         return merging(dict) { return $1 }
     }
     
-    mutating func pick(for key: Key) -> Value? {
-        if let val = self[key] {
-            self.removeValue(forKey: key)
-            return val
-        }
-        return nil
-    }
-    
     func picking(for key: Key) -> (Self, Value)? {
         if let val = self[key] {
             var dict = self
+            
             dict.removeValue(forKey: key)
             return (dict, val)
         }
         return nil
+    }
+    
+    @discardableResult
+    mutating func remove<Seq: Sequence>(for keys: Seq) -> Self where Seq.Element == Key {
+        var removed = [Key : Value]()
+        
+        keys.forEach() { key in
+            if let val = removeValue(forKey: key) {
+                removed[key] = val
+            }
+        }
+        return removed
+    }
+    
+    func removing<Seq: Sequence>(for keys: Seq) -> (rest: Self, removed: Self) where Seq.Element == Key {
+        var rest = self
+        let removed = rest.remove(for: keys)
+        
+        return (rest: rest, removed: removed)
+    }
+    
+    //MARK: deprecated
+    
+    /// - Warning: **Deprecated**. Use `init(size:buildBlock:)` instead.
+    @available(*, deprecated, message: "Use `removeValue(forKey:)` instead")
+    mutating func pick(for key: Key) -> Value? {
+        return removeValue(forKey: key)
     }
 }
 
