a790c3f4f3fc92cd1884b90da8c801af638e548f
Stanislav Dmytriiev <stanislav.dmytriiev@shortcut.no>
2022-04-26 17:59:16 +0200

    Add docs for Chain Executor

--- Sources/SSSugarCore/Classes/SSChainExecutor.swift
+++ Sources/SSSugarCore/Classes/SSChainExecutor.swift
@@ -1,22 +1,41 @@
 import Foundation
 
+/// Requirements for tool that chain async tasks execution.
 public protocol SSChainExecuting {
+    /// Executors Handler type
     typealias Handler = () -> Void
+    /// Executors task type
     typealias Task = (@escaping Handler) -> Void
     
+    /// Adds passed task to end of chain
+    /// - Parameter task: task to add
+    /// - Returns: Object was called on.
     @discardableResult
     func add(_ task: @escaping Task) -> Self
+    
+    /// Runs added tasks one by one and calls handler on finish.
+    /// - Parameter executor: Executor to call handler on.
+    /// - Parameter handler: Finish handler to be called on last task finished.
     func finish(executor: SSExecutor, _ handler: @escaping Handler)
+    
+    /// Runs added tasks one by one and calls handler on finish.
+    /// - Parameter handler: Finish handler to be called on last task finished.
     func finish(_ handler: @escaping Handler)
+    
+    /// Runs added tasks one by one
     func finish()
 }
 
-/// Note: - SSChainExecutor willn't produce memory leak, even if some of adding tasks willn't call handler or somewhat else, cuz logic in finish method doesn't captures `self` at all.
+/// Executor that executes async tasks one by one.
+///
+/// Note: - DefaultChainExecutor willn't produce memory leak, even if some of adding tasks willn't call handler or somewhat else, cuz logic in finish method doesn't captures `self` at all.
 public class SSChainExecutor: SSChainExecuting {
     public typealias Task = (@escaping Handler) -> Void
     
-    public var tasks = [Task]()
+    /// Stored tasks to execute
+    private var tasks = [Task]()
     
+    /// Creates new chain executor
     public init() {}
 
     @discardableResult
@@ -29,6 +48,9 @@ public class SSChainExecutor: SSChainExecuting {
         finish(executor: executor, handler: handler)
     }
     
+    /// See `ChainExecutor.finish()` docs.
+    ///
+    /// - Note: Handler will be called within the same queue as handler of last added task.
     public func finish(_ handler:  @escaping Handler) {
         finish(executor: nil, handler: handler)
     }
