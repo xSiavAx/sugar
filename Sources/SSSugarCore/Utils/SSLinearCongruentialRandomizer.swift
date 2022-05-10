a064e9cc4dcb95f5c42793dfcb20c7d14e28fbfa
Stanislav Dmytriiev <mail@siava.pp.ua>
2022-05-10 16:42:47 +0200

    Deprecate LinearCongerentRandomizer

--- Sources/SSSugarCore/Utils/SSLinearCongruentialRandomizer.swift
+++ Sources/SSSugarCore/Utils/SSLinearCongruentialRandomizer.swift
@@ -1,16 +1,11 @@
 import Foundation
 
-/// Linear Congruential Random implementation. Class conforms to `SSLinearCongruentialGenerator`.
-/// May be initialized with seed and algorithm params (divisor, multiplier, additional) or with default values.
-/// - Note: Using default param values is recomended, otherwise read algorithm description for accurate values picking.
+/// - Warning: **Deprecated**. Use `init(size:buildBlock:)` instead.
+@available(*, deprecated, message: "Works badly. Use GKMersenneTwisterRandomSource from GameplayKit instead.")
 public struct SSLinearCongruentialRandomizer {
-    /// Params used within algorithm implementation.
     public struct Params {
-        /// Algorith parameter
         public let divisor: Double
-        /// Algorith parameter
         public let multiplier: Double
-        /// Algorith parameter
         public let additional: Double
         
         public init(divisor mDivisor: Double, multiplier mMultiplier: Double, additional mAdditional: Double) {
@@ -29,46 +24,10 @@ public struct SSLinearCongruentialRandomizer {
     
     /// Algorithm parameters
     public private(set) var params: Params
-    
-    /// Create new randomizer instance.
-    /// - Parameters:
-    ///   - seed: Starting seed. Default value are based on current timestamp
-    ///   - params: Algorithm parameters. Default value provided
+
     public init(withSeed seed : Double = defaultSeed,
          params mParams: Params = defaultParams) {
         mutatedSeed = seed
         params = mParams
-    }
-    
-    /// Generate random value in range [0, 1)
-    static public func random(seed: Double = defaultSeed) -> Double {
-        var randomizer = SSLinearCongruentialRandomizer(withSeed: seed)
-        return randomizer.nextNormilized()
-    }
-    
-    /// Generate random value in range [0, 1)
-    public mutating func nextNormilized() -> Double {
-        let seed = params.applying(to: mutatedSeed)
-        
-        mutatedSeed = seed.seed
-        return seed.normilized
-    }
-}
-
-extension SSLinearCongruentialRandomizer: RandomNumberGenerator {
-    public mutating func next() -> UInt64 {
-        return UInt64(nextNormilized() * Double(UInt64.max))
-    }
-
-    @available(swift, deprecated: 5.5, message: "RandomNumberGenerator protocol requirements changes from generic to UInt64 for return type.")
-    public mutating func next<T>() -> T where T : FixedWidthInteger, T : UnsignedInteger {
-        return T(nextNormilized() * Double(T.max))
-    }
-}
-
-extension SSLinearCongruentialRandomizer.Params {
-    func applying(to seed: Double) -> (seed:Double, normilized:Double) {
-        let newSeed = ((seed * multiplier + additional).truncatingRemainder(dividingBy:divisor))
-        return (newSeed, newSeed / divisor)
     }
 }
