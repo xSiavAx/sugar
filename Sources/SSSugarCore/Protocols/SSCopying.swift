384ec2190d554bc0a783185778845a4121e676cc
Stanislav Dmytriiev <mail@siava.pp.ua>
2022-05-31 14:58:34 -0400

    Add TODOs

--- Sources/SSSugarCore/Protocols/SSCopying.swift
+++ Sources/SSSugarCore/Protocols/SSCopying.swift
@@ -1,5 +1,8 @@
 import Foundation
 
+#warning("Remove me")
+// It's unsafe to use copy on objects and causes a lot of problems. If u need copyable use Value Types.
+
 public protocol SSCopying: AnyObject {
     init(copy other: Self)
     func copy() -> Self
